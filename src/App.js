f13413798cb47c5e523eb1805dda8e8af8883f52
launchpad5682 <launchpad5682@gmail.com>
2021-10-27 23:38:46 +0530

    Data Store Provider added

--- src/App.js
+++ src/App.js
@@ -2,33 +2,15 @@ import "./App.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Board from "./Board";
+import { DataStoreProvider } from "./DataLayer";
 
 function App() {
-  const boardLists = [
-    {
-      boardId: "1",
-      boardName: "To-Do",
-      list: [
-        { data: "Complete to-do implementation" },
-        { data: "Figure out React-DnD" },
-      ],
-    },
-    {
-      boardId: "2",
-      boardName: "Doing",
-      list: [{ data: "Trying to figure out the React-DnD implementation" }],
-    },
-    {
-      boardId: "3",
-      boardName: "Done",
-      list: [{ data: "Referred React-DnD documentation" }],
-    },
-  ];
-
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Board boardLists={boardLists} />
-    </DndProvider>
+    <DataStoreProvider>
+      <DndProvider backend={HTML5Backend}>
+        <Board />
+      </DndProvider>
+    </DataStoreProvider>
   );
 }
 
